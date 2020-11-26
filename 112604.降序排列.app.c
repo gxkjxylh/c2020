cd5b28ec68f36ca8db2f7cc3e1b64abb2a31ed26
gxkjxylh <74294813+gxkjxylh@users.noreply.github.com>
2020-11-26 20:36:36 +0800

    Create 112604.降序排列.app.c

---
+++ 112604.降序排列.app.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int n,a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+if(a<b)
+{n=a,a=b,b=n;}
+if(a<c)
+{n=a,a=c,c=n;}
+if(b<c){
+	n=b,b=c,c=n;}
+
+printf("%d %d %d\n",a,b,c);
+
+}
