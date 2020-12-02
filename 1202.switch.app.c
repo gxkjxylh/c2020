c172ede6267afbfe014cf29c64d2a225f22e4faa
gxkjxylh <74294813+gxkjxylh@users.noreply.github.com>
2020-12-02 08:37:48 +0800

    Create 1202.switch.app.c

---
+++ 1202.switch.app.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+	int n;
+scanf("%d",&n);
+switch(n)
+{case 1:printf("n的值为1");break;
+case 2:printf("n的值为2");break;
+case 3:printf("n的值为3");break;
+default :printf("n的值无效");}
+}
