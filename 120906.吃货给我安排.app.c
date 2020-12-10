525b6dbdf74bd785365719191e05b20440bbf852
gxkjxylh <74294813+gxkjxylh@users.noreply.github.com>
2020-12-10 19:51:34 +0800

    Update and rename 112.c to 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -5,16 +5,15 @@ void main(){
 	char c[]="www";
 	printf("吃货的日程总是被安排的明明白白，请问现在是什么节？\n");
 	scanf("%s",&c);
-	if(strcmp(c,"春节")){
+	if(strcmp(c,"春节")==0){
 	printf("给我安排饺子。\n");}
-    else if(strcmp(c,"端午节")){
+    else if(strcmp(c,"端午节")==0){
 	printf("给我安排粽子。\n");}
-    else if(strcmp(c,"中秋节")){
+    else if(strcmp(c,"中秋节")==0){
 	printf("给我安排月饼。\n");}
-    else if(strcmp(c,"元宵节")){
+    else if(strcmp(c,"元宵节")==0){
 	printf("给我安排汤圆。\n");}
-
-
-
-
 }
+
+
+
